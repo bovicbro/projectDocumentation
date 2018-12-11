0a7055faa20e8c3777db328a8854c57c6868eb2b
Victor Broman <Victor.bo.broman@gmail.com>
2018-12-11 21:19:31 +0100

    Adds card design to expandedProject component

--- src/components/ExpandedProject.js
+++ src/components/ExpandedProject.js
@@ -4,8 +4,8 @@ import './expandedProject.css';
 class ExpandedProject extends Component {
   render() {
 	return (
-	  <div className="ExpandedProject">
-	  		<div className="ProjectName">{this.props.name}</div>
+	  <div className="expandedProject">
+	  		<div className="ProjectName">{this.props.label}</div>
 	  </div>
 	)
   }
