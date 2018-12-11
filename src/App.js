6fee8b02cefcb672e02a9794c2994a19474aede9
Marcus Bodin <mbod@sto-m-mbod.local>
2018-12-11 20:09:03 +0100

    some css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="appHeader">
+          <h1>Project Documentation</h1>
+        </div>
         <ProjectGrid />
       </div>
     );
--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './projectGrid.css';
 
 class ProjectGrid extends Component {
   render() {
-return (
-  <p>Hello World</p>
-)
+	return (
+	  <div className="projectGrid">
+	  	<div className="projectGridHeader">
+	  		<div className="projectRow">Project</div>
+	  		<div className="projectRow">Value 1</div>
+	  		<div className="projectRow">Value 2</div>
+	  	</div>
+	  </div>
+	)
   }
 }
 
