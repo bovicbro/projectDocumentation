3e733a8a35c7c6983f69e08831e62cccab50cc06
Marcus Ahlberg <marcus.ahlberg@netlight.com>
2019-01-09 19:46:48 +0100

    lot of things

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,32 +1,39 @@
-import {connect} from 'react-redux';
-import React, { Component } from 'react';
-import './projectGrid.css';
-import ProjectGridRow from './ProjectGridRow'
+import { connect } from "react-redux"
+import React, { Component } from "react"
+import "./projectGrid.css"
+import ProjectGridRow from "./ProjectGridRow"
 
 class ProjectGrid extends Component {
   render() {
-	const projects=this.props.projects
-	const fields=this.props.fields
-	return (
-	  <div className="projectGrid">
-	  	<div className="projectGridRow" id="projectGridHeader">
-	  	{fields.map(field=>{
-	  		return (
-		  		<div className="projectGridColumn">{field.label}</div>
-		  	)
-	  	})}
-	  	</div>
-	  	{projects.map(project=><ProjectGridRow project={project}/>)}
-	  </div>
-	)
+    const projects = this.props.projects
+    const fields = this.props.fields
+    return (
+      <div className="projectGrid">
+        <div className="projectGridRow" id="projectGridHeader">
+          <div className="projectGridColumn">Project name</div>
+          {fields
+            .filter(field => field.isColumn)
+            .map(field => {
+              return (
+                <div className="projectGridColumn" key={field._id}>
+                  {field.label}
+                </div>
+              )
+            })}
+        </div>
+        {projects.map(project => (
+          <ProjectGridRow project={project} fields={fields} key={project._id} />
+        ))}
+      </div>
+    )
   }
 }
 
-const mapStateToProps = (state)=>{
-	return{
-		projects:state.projects,
-		fields:state.fields.filter(field=>field.isColumn)
-	}
-};
+const mapStateToProps = state => {
+  return {
+    projects: state.projects,
+    fields: state.fields
+  }
+}
 
-export default connect(mapStateToProps)(ProjectGrid);
+export default connect(mapStateToProps)(ProjectGrid)
--- src/components/ProjectGridRow.js
+++ src/components/ProjectGridRow.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import ExpandedProject from "./ExpandedProject"
 //import './projectGridRow.css';
 
+const findProjectValue = (project, fieldId) => {
+  const value = project.values.find(
+    fieldValue => fieldValue.fieldId === fieldId
+  )
+
+  return value ? value.value : null
+}
+
 class ProjectGridRow extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { expanded: false }
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState({
+      expanded: !this.state.expanded
+    })
+  }
+
+  renderExpanded() {
+    if (this.state.expanded) {
+      return <ExpandedProject label={this.props.project.label} />
+    }
+    return null
+  }
+
   render() {
-	return (
-	  	<div className="projectGridRow">
-	  		<div className="projectGridColumn">{this.props.project.label}</div>
-	  	</div>
-	)
+    return (
+      <div className="projectGridRow" onClick={this.toggle}>
+        <div className="projectGridColumn">{this.props.project.label}</div>
+        {this.props.fields
+          .filter(field => field.isColumn)
+          .map(field => (
+            <div className="projectGridColumn" key={field._id}>
+              {findProjectValue(this.props.project, field._id)}
+            </div>
+          ))}
+        {this.renderExpanded()}
+      </div>
+    )
   }
 }
 
-export default (ProjectGridRow);
+export default ProjectGridRow
