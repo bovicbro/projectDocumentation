65dd29e316ad838bff08a4b8c39361dd88147353
Marcus Ahlberg <marcus.ahlberg@netlight.com>
2019-01-09 18:12:10 +0100

    make syntax easier

--- src/store/actions/loadData.js
+++ src/store/actions/loadData.js
@@ -1,13 +1,19 @@
 import { LOAD_PROJECTS, LOAD_FIELDS } from "./actionTypes"
 
-export const fetchProjects = () => dispatch => {
-  return fetch("data/projects.json")
-    .then(res => res.json())
-    .then(projects => dispatch({ type: LOAD_PROJECTS, payload: { projects } }))
+export const fetchProjects = () => {
+  return dispatch => {
+    fetch("data/projects.json")
+      .then(res => res.json())
+      .then(projects =>
+        dispatch({ type: LOAD_PROJECTS, payload: { projects } })
+      )
+  }
 }
 
-export const fetchFields = () => dispatch => {
-  return fetch("data/fields.json")
-    .then(res => res.json())
-    .then(fields => dispatch({ type: LOAD_FIELDS, payload: { fields } }))
+export const fetchFields = () => {
+  return dispatch => {
+    fetch("data/fields.json")
+      .then(res => res.json())
+      .then(fields => dispatch({ type: LOAD_FIELDS, payload: { fields } }))
+  }
 }
