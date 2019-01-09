48a269bb6e35f3b0393f6a084dfa3ee9307bca55
Victor Broman <Victor.bo.broman@gmail.com>
2019-01-09 20:38:36 +0100

    All fields to expanded project, even if the project does not have values for that field

--- src/components/ExpandedProject.js
+++ src/components/ExpandedProject.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-import './expandedProject.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./expandedProject.css";
+import ProjectUtil from "../utils/ProjectUtil";
 
 class ExpandedProject extends Component {
   render() {
-	return (
-	  <div className="expandedProject">
-	  		<div className="ProjectName">{this.props.project.label}</div>
-        {this.props.project.values.map(field => (
-          <div className='ProjectValue'>{field.value}</div>
+    return (
+      <div className="expandedProject">
+        <div className="ProjectName">{this.props.project.label}</div>
+        {this.props.fields.map(field => (
+          <div className="ProjectValue">
+            <strong>{field.label}</strong>
+            {ProjectUtil.getFieldValue(this.props.project, field._id)}
+          </div>
         ))}
-	  </div>
-	)
+      </div>
+    );
   }
 }
 
 ExpandedProject.propTypes = {
-  project: PropTypes.object.isRequired
-}
+  project: PropTypes.object.isRequired,
+  fields: PropTypes.array.isRequired
+};
 
 export default ExpandedProject;
